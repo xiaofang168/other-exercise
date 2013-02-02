0ff4b4544f723bb5ebc0c9f90eb8e794389d96ed
fangj <fangj@a476048d-a9f5-44f3-a69e-62e170b0d24d>
2013-02-02 13:18:07 +0000

    数组去重，行字处理去重

---
+++ src/main/perl/exercise/file_txt.pl
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+
+
+my $array_new=[];
+open(IN,"text.txt") or die "Cant't open file $!";
+open(OUT,">text2.txt");
+while(<IN>){
+	chomp;
+	my($a1,$a2)=split(/\t/,$_);
+	my @array=($a1,$a2);
+	@array=sort(@array);
+	push $array_new,[@array];
+    print OUT join("\t",sort(@array)),"\n";
+}
+close IN;
+close OUT;
+print "finish!","\n";
+
+#遍历新数组元素
+
+#print scalar(@{$array_new});
+my @array =@{$array_new};
+print "Remove duplicate ago\n";
+printArray (@array);
+print "\n";
+
+
+my %hash;
+#grep 保存符合条件的元素
+@array = grep { ++$hash{$_->[0].$_->[1]} < 2 } @array;
+print "Remove duplicate after\n";
+printArray (@array);
+print "\n";
+
+sub printArray{
+	my $tmp;
+	foreach $tmp (@_){
+	 	print $tmp->[0]."\t".$tmp->[1]."\n";
+	}
+}
