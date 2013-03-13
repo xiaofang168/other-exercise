127fa76a959066792a6e885fa411bcdd5c4f134b
fangj <fangj@a476048d-a9f5-44f3-a69e-62e170b0d24d>
2013-03-13 07:13:15 +0000

    增加了backbone coffee 练习

---
+++ trunk/src/main/backbone-coffee/example/page.js
@@ -0,0 +1,19 @@
+var page={
+form:{
+	id:"",
+	name:"",
+	target:"_self",
+	enctype:"",
+	backround:""
+},
+table:{
+		   id:"",
+		   tr:[{
+
+		       td:[{
+		    		 content:""}]
+		   }
+     ]      
+
+}	
+}
