d53dfb7c95a746be27d6d99c478a07fecef72934
fangj <fangj@a476048d-a9f5-44f3-a69e-62e170b0d24d>
2013-04-23 12:14:07 +0000

    增加了练习

--- src/main/perl/exercise/hello.pl
+++ src/main/perl/exercise/hello.pl
@@ -3,3 +3,34 @@ use strict;
 use warnings;
 
 print "Hello World\n";
+
+my @array=("print","these","strings","out","for","me");
+print  $array[0];
+print   "\n";
+#foreach 
+foreach(@array){
+	print $_."\n";
+}
+#out length
+print "length:".(scalar @array)."\n";
+print "length method 2".(@array),"\n";
+# "最后一个索引的号码是 5"
+print "The last populated index is ".$#array,"\n";
+
+
+#Perl 脚本捕获的命令行参数被保存在内置的数组变量 @ARGV 中
+# "print these strings out for me"
+print "@array";
+
+#
+print "Hello \$string"; # "Hello $string"
+print 'Hello $string';  # "Hello $string"
+print "\@array";        # "@array"
+print '@array';         # "@array"
+
+#for
+for(my $i = 0; $i < scalar @array; $i++) {
+	print $i, ": ", $array[$i];
+}
+#使用负数索引来检索从后往前的元素：
+print	($array[-1]);
