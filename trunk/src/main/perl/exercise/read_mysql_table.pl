7c9596c6fad26f7a73a17771a7b3d88471ef2d8e
fangj <fangj@a476048d-a9f5-44f3-a69e-62e170b0d24d>
2013-07-09 14:44:59 +0000

    修改了标量

--- trunk/src/main/perl/exercise/read_mysql_table.pl
+++ trunk/src/main/perl/exercise/read_mysql_table.pl
@@ -4,6 +4,8 @@ use warnings;
 use Template;
 use Encode;
 use DBI;
+use Perl6::Slurp;
+use autodie; 
 
 #defin typeMap
 my %filed_type = (
@@ -14,35 +16,39 @@ my %filed_type = (
 );
 
 
-our $array=[];
+our @array=();
 # connect
-my $dbh = DBI->connect("DBI:mysql:test:localhost:3306", "root", "");
+my $dbh = DBI->connect("DBI:mysql:test:localhost:", "", "");
 
-my $sth = $dbh->prepare("SELECT column_name,data_type,column_default,is_nullable FROM information_schema.COLUMNS WHERE TABLE_NAME = 'menu'");
+my $sth = $dbh->prepare("SELECT column_name,data_type,column_default,is_nullable FROM information_schema.COLUMNS WHERE TABLE_NAME = 'resource'");
 $sth->execute();
 # iterate through resultset
 # print values
 while(my @data = $sth->fetchrow_array()) {
-   push $array, {col_name=>$data[0],col_type=>$filed_type{$data[1]},col_default=>$data[2],col_nullable=>$data[3]};
+   push @array, {col_name=>$data[0],col_type=>$filed_type{$data[1]},col_default=>$data[2],col_nullable=>$data[3]};
 }
+
+for my $element (@array) {  
+  		#print $element->{col_name}."\n";
+}
+
 # 建一个 TT 的对象
 my $tt = Template->new({ INCLUDE_PATH => '.',  INTERPOLATE  => 1, }) || die "$Template::ERROR\n";       
 my $vars = {       
 	table_name => "Menu", 
-	teams=>$array
+	teams=>\@array
 };   
 # 使用 
 my $templateFile = 'module.tt'; 
-my $outputContent=""; 
+my $outputContent; 
 $tt->process($templateFile, $vars, "Menu.rb") || die $tt->error();
-$tt->process($templateFile, $vars, $outputContent) || die $tt->error();
-#print $outputContent;
+#$tt->process($templateFile, $vars, $outputContent) || die $tt->error();
+my $text_file = 'Menu.rb';   
+open my $text, '<', $text_file or die $!;   
+my $content = slurp $text;  
+#replace last , 
+$content=~s/,([^,]*)$/$1/;
 
-my $test="a,b,c,d,e,end";
-#$test=~s/,$//;
-#$test=~s/,([^,]*)$/$1/;
-#$test=~s/,end$//;
-#$outputContent=~s/(.+),/$1/s;
-$outputContent=~s/,\n*end\n*$//;
-print $outputContent;
-print ">>>\n"
+open(HANDLE, ">",$text_file) or die "Can't open $text_file $!";
+print HANDLE $content;  
+close(HANDLE);  
