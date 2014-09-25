d13452aa4ec80880ab9c5956e1ab02465c6629bc
fangj <fangj@a476048d-a9f5-44f3-a69e-62e170b0d24d>
2014-09-25 15:13:43 +0000

    增加了正则表达式分割的例子

--- trunk/src/main/perl/exercise/regex_word.pl
+++ trunk/src/main/perl/exercise/regex_word.pl
@@ -1,6 +1,16 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+
 my $_="ABCDEFGHIGKLMN";
 my $str = $& if /(?<=C).*(?=L)/;
-print $str."\n"
+print $str."\n";
+#my @a = split(/(\d+)/, "as12f31jd");
+#my @b = split(/([a-z])/,"jd");
+#print $_,"\n" for @a;
+#print $_,"\n" for @b;
+my @a=split/(?<=\d)(?!\d)|(?<!\d)(?=.)/,"as12f31jd";
+print $_,"\n" for @a;
+
+
