883714f9dc30009fb63609a6c88402490ddbb0c4
fangj <fangj@a476048d-a9f5-44f3-a69e-62e170b0d24d>
2014-09-29 14:53:41 +0000

    增加了快速创建数组的方法

--- trunk/src/main/perl/exercise/array_test.pl
+++ trunk/src/main/perl/exercise/array_test.pl
@@ -12,5 +12,23 @@ print $a->[0],"\n";
 my @array = (1, 2, 3, 4, 5); 
 my $array_r = \@array; 
 #拷贝了数组
-my @array2 = @{$array_r};  
+my @array2 = @{$array_r};
+
+my @names=qw/fred barney betty dino wilma pebbles bamm-bamm/;
+my $result = &which_element_is("dino",@names);
+print $result;
+sub which_element_is{
+	my($what,@array)=@_;
+	print($what."\n");
+	print(@array."\n");
+	# $#array 是数组的最后一个元素的下标索引
+	foreach(0..$#array){
+		if($what eq $array[$_]){
+			return $_;
+		}
+	}
+	-1;
+}
+
+
 
