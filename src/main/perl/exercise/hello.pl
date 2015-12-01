60144e16369d653fc6b1d0ac01d7884377a5581f
fangjie <FangJie1988>
2015-12-01 15:56:21 +0800

    added say map grep and dumper

--- src/main/perl/exercise/hello.pl
+++ src/main/perl/exercise/hello.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use 5.014;
+use Data::Dumper;
 # defined
 my ($col,$row) = 0;
 
@@ -37,3 +39,14 @@ for(my $i = 0; $i < scalar @array; $i++) {
 #使用负数索引来检索从后往前的元素：
 print	($array[-1]);
 print "hello word" x 2; 
+
+my @list1 = (4..9,'d'..'w');
+my @list2 = (0..9,'a'..'z');
+my %hash = map{$_ => 1}@list1;
+my @sss = grep{$hash{$_}}@list2;
+say for @sss;
+
+print "\n\n";
+my @names = qw(Foo Bar Baz);
+my %is_invited = map {$_ => 1} @names;
+print Dumper \%is_invited;
