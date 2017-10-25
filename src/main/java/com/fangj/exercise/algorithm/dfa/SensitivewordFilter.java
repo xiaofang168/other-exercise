ff96d1883b249ddc4b44981e72e88704139f9126
fangjie <fangjie@etouch.cn>
2017-10-25 16:24:43 +0800

    Enhancement:added algorithm dfa

---
+++ src/main/java/com/fangj/exercise/algorithm/dfa/SensitivewordFilter.java
@@ -0,0 +1,185 @@
+package com.fangj.exercise.algorithm.dfa;
+
+import java.util.*;
+
+/**
+ * @author fangjie
+ * @date Created in 下午3:27 17/10/25.
+ */
+public class SensitivewordFilter {
+
+    /**
+     * 最小匹配规则
+     */
+    public static int minMatchTYpe = 1;
+    private Map sensitiveWordMap;
+
+    /**
+     * 构造函数，初始化敏感词库
+     */
+    public SensitivewordFilter() {
+        Set<String> set = new HashSet<>();
+        set.add("三级片");
+        set.add("法轮功");
+        set.add("王八蛋");
+        set.add("王八羔子");
+        sensitiveWordMap=addSensitiveWordToHashMap(set);
+    }
+
+    /**
+     * 读取敏感词库，将敏感词放入HashSet中，构建一个DFA算法模型：<br>
+     */
+    private Map addSensitiveWordToHashMap(Set<String> wordSet) {
+        // 初始化敏感词容器，减少扩容操作
+        Map wordMap = new HashMap(wordSet.size());
+        for (String word : wordSet) {
+            Map nowMap = wordMap;
+            for (int i = 0; i < word.length(); i++) {
+                // 转换成char型
+                char keyChar = word.charAt(i);
+                // 获取
+                Object tempMap = nowMap.get(keyChar);
+                // 如果存在该key，直接赋值
+                if (tempMap != null) {
+                    nowMap = (Map) tempMap;
+                }
+                // 不存在则，则构建一个map，同时将isEnd设置为0，因为他不是最后一个
+                else {
+                    // 设置标志位
+                    Map<String, String> newMap = new HashMap<>();
+                    newMap.put("isEnd", "0");
+                    // 添加到集合
+                    nowMap.put(keyChar, newMap);
+                    nowMap = newMap;
+                }
+                // 最后一个
+                if (i == word.length() - 1) {
+                    nowMap.put("isEnd", "1");
+                }
+            }
+        }
+        return wordMap;
+    }
+
+    /**
+     * 获取文字中的敏感词
+     *
+     * @param txt       文字
+     * @param matchType 匹配规则&nbsp;1：最小匹配规则，2：最大匹配规则
+     * @return
+     * @author chenming
+     * @date 2014年4月20日 下午5:10:52
+     * @version 1.0
+     */
+    public Set<String> getSensitiveWord(String txt, int matchType) {
+        Set<String> sensitiveWordList = new HashSet<>();
+        for (int i = 0; i < txt.length(); i++) {
+            //判断是否包含敏感字符
+            int length = checkSensitiveWord(txt, i, matchType);
+            //存在,加入list中
+            if (length > 0) {
+                sensitiveWordList.add(txt.substring(i, i + length));
+                //减1的原因，是因为for会自增
+                i = i + length - 1;
+            }
+        }
+        return sensitiveWordList;
+    }
+
+    /**
+     * 检查文字中是否包含敏感字符
+     *
+     * @param txt
+     * @param beginIndex
+     * @param matchType
+     * @return，如果存在，则返回敏感词字符的长度，不存在返回0
+     * @version 1.0
+     */
+    public int checkSensitiveWord(String txt, int beginIndex, int matchType) {
+        //敏感词结束标识位：用于敏感词只有1位的情况
+        boolean flag = false;
+        //匹配标识数默认为0
+        int matchFlag = 0;
+        char word;
+        Map nowMap = sensitiveWordMap;
+        for (int i = beginIndex; i < txt.length(); i++) {
+            word = txt.charAt(i);
+            nowMap = (Map) nowMap.get(word);
+            if (nowMap != null) {
+                matchFlag++;
+                if ("1".equals(nowMap.get("isEnd"))) {
+                    flag = true;
+                    //最小规则，直接返回,最大规则还需继续查找
+                    if (SensitivewordFilter.minMatchTYpe == matchType) {
+                        break;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        //长度必须大于等于1，为词
+        if (matchFlag < 2 || !flag) {
+            matchFlag = 0;
+        }
+        return matchFlag;
+    }
+
+    /**
+     * 获取替换字符串
+     * @author chenming
+     * @date 2014年4月20日 下午5:21:19
+     * @param replaceChar
+     * @param length
+     * @return
+     * @version 1.0
+     */
+    private String getReplaceChars(String replaceChar,int length){
+        String resultReplace = replaceChar;
+        for(int i = 1 ; i < length ; i++){
+            resultReplace += replaceChar;
+        }
+        return resultReplace;
+    }
+
+    /**
+     * 替换敏感字字符
+     * @param txt
+     * @param matchType
+     * @param replaceChar 替换字符，默认*
+     * @version 1.0
+     */
+    public String replaceSensitiveWord(String txt,int matchType,String replaceChar){
+        String resultTxt = txt;
+        Set<String> set = getSensitiveWord(txt, matchType);     //获取所有的敏感词
+        Iterator<String> iterator = set.iterator();
+        String word = null;
+        String replaceString = null;
+        while (iterator.hasNext()) {
+            word = iterator.next();
+            replaceString = getReplaceChars(replaceChar, word.length());
+            resultTxt = resultTxt.replaceAll(word, replaceString);
+        }
+        return resultTxt;
+    }
+
+
+
+    public static void main(String[] args) {
+        SensitivewordFilter filter = new SensitivewordFilter();
+        System.out.println("敏感词库："+filter.sensitiveWordMap);
+        System.out.println("敏感词的数量：" + filter.sensitiveWordMap.size());
+        String txt = "王八蛋太多的伤感情怀也许只局限于饲养基地 荧幕中的情节，主人公尝试着去用某种方式渐渐的很潇洒地释自杀指南怀那些自己经历的伤感。"
+                + "然后法轮功，王八羔子，我们的扮演的角色就是跟随着主人公的喜红客联盟 怒哀乐而过于牵强的把自己的情感也附加于银幕情节中，然后感动就流泪，"
+                + "难过就躺在某一个人的怀里尽情的王八阐述心扉或者手机卡复制器一个人一杯红酒一部电影在夜三级片 深人静的晚上，关上电话静静的发呆着。";
+        System.out.println("待检测语句字数：" + txt.length());
+        System.out.println("原语句："+txt);
+        long beginTime = System.currentTimeMillis();
+        Set<String> set = filter.getSensitiveWord(txt, 1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("语句中包含敏感词的个数为：" + set.size() + "。包含：" + set);
+        System.out.println("替换后的语句："+filter.replaceSensitiveWord(txt,minMatchTYpe,"*"));
+        System.out.println("总共消耗时间为：" + (endTime - beginTime));
+    }
+
+}
