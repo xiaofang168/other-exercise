70b030c66ef7e4e41b1c5c6f29fc07cba896b451
fangjie <fangjie@etouch.cn>
2017-12-04 17:32:45 +0800

    added MergeSort

---
+++ src/main/java/com/fangj/exercise/algorithm/MergeSort.java
@@ -0,0 +1,86 @@
+package com.fangj.exercise.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @author fangjie
+ * @date Created in 下午4:43 17/11/29.
+ */
+public class MergeSort {
+
+    public static void merge(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        // 左指针
+        int i = low;
+        // 右指针
+        int j = mid + 1;
+        int k = 0;
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
+        }
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = a[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        for (int k2 = 0; k2 < temp.length; k2++) {
+            a[k2 + low] = temp[k2];
+        }
+    }
+
+    public static void mergeSort(int[] a, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            // 左边
+            mergeSort(a, low, mid);
+            // 右边
+            mergeSort(a, mid + 1, high);
+            // 左右归并
+            merge(a, low, mid, high);
+            System.out.println(Arrays.toString(a));
+        }
+
+    }
+
+    public static int[] merge(int a[], int b[]) {
+        int[] result = new int[a.length + b.length];
+        // i:用于标示a数组    j：用来标示b数组  k：用来标示传入的数组
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] <= b[j]) {
+                result[k++] = a[i++];
+            } else {
+                result[k++] = b[j++];
+            }
+        }
+        /* 后面连个while循环是用来保证两个数组比较完之后剩下的一个数组里的元素能顺利传入 */
+        while (i < a.length) {
+            result[k++] = a[i++];
+        }
+        while (j < b.length) {
+            result[k++] = b[j++];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 51, 46, 20, 18, 65, 97, 82, 30, 77, 50 };
+        mergeSort(a, 0, a.length - 1);
+        int[][] bb = {{10, 15, 30}, {12, 15, 20}, {17, 20, 32}};
+        int[] b1= {10, 15, 30};
+        int[] b2 = {12, 15, 20};
+        System.out.println("排序结果：" + Arrays.toString(a));
+        mergeSort(b1,0, bb.length-1);
+        System.out.println(Arrays.toString(merge(b1,b2)));
+    }
+
+}
