1ecf87f1e16343f227287900c406643c3dfbbf0d
fangjie <fangjie@etouch.cn>
2018-06-09 12:53:19 +0800

    added haskell

---
+++ src/main/haskell/hello.hs
@@ -0,0 +1,9 @@
+main::IO()
+aa::IO()
+aa = do 
+    print "Hello World!"
+main = do
+    aa
+    putStrLn "Greetings!  What is your name?"
+
+
---
+++ src/main/haskell/monad_io.hs
@@ -0,0 +1,7 @@
+module Main where
+import Data.Char (toUpper)
+main :: IO ()
+toUpperCase :: String -> String
+toUpperCase = map toUpper
+-- main = putStrLn "input something" >>= (\_ -> getLine) >>= putStrLn . toUpperCase
+main = putStrLn "input something" >> getLine >>= putStrLn . toUpperCase
