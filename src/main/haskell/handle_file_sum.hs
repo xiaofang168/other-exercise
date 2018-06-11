7b5f6fc82c930e88ba963da704f6c16a0cf8e5ef
fangjie <fangjie@etouch.cn>
2018-06-11 18:44:59 +0800

    read data from txt and sum

---
+++ src/main/haskell/handle_file_sum.hs
@@ -0,0 +1,21 @@
+import Data.List.Split
+
+main::IO()
+-- readLines = fmap lines . readFile
+-- main = do
+    -- putStrLn(show(length (splitOn " " "asdf adfss")))
+    -- contents <- readFile "p2p.txt"
+    -- let ls = lines contents
+    -- mapM_ putStrLn ls
+    -- let numberStrArray = map (\x -> (splitOn " " x)!!1) ls
+    -- let numberArray = map (\x -> read x :: Integer ) numberStrArray
+    -- let s =sum(numberArray)
+    -- print((show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) ls)
+    -- print $ show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1) $ lines contents
+
+
+-- main = readFile "p2p.txt" >>= (\inpStr -> putStrLn(((show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))))
+
+main = readFile "p2p.txt" >>= (\inpStr -> putStrLn(((show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))))
+    
+    
