7dfe71dbed18206ace29453154d139f692198fe7
fangjie <fangjie@etouch.cn>
2018-06-14 16:07:22 +0800

    use where syntax

--- src/main/haskell/handle_file_sum.hs
+++ src/main/haskell/handle_file_sum.hs
@@ -15,7 +15,10 @@ main::IO()
 
 
 -- main = readFile "p2p.txt" >>= (\inpStr -> putStrLn(((show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))))
-
-main = readFile "p2p.txt" >>= (\inpStr -> (putStrLn . show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))
+-- main = readFile "p2p.txt" >>= (\inpStr -> (putStrLn . show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))
+main = readFile "p2p.txt" >>= printLines . lines
+ where covInt s = read s :: Integer
+       getNum x = (splitOn " " x)!!1
+       printLines x = (putStrLn . show . sum . map covInt . map getNum) x
     
     
