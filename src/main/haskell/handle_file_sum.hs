f2b33b46e652961f8ca3aae7d4ce59336467ed7f
fangjie <fangjie@etouch.cn>
2018-06-14 16:25:23 +0800

    refactor

--- src/main/haskell/handle_file_sum.hs
+++ src/main/haskell/handle_file_sum.hs
@@ -16,9 +16,15 @@ main::IO()
 
 -- main = readFile "p2p.txt" >>= (\inpStr -> putStrLn(((show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))))
 -- main = readFile "p2p.txt" >>= (\inpStr -> (putStrLn . show . sum . map (\x -> read x :: Integer ) . map (\x -> (splitOn " " x)!!1)) (lines inpStr))
+{-
 main = readFile "p2p.txt" >>= printLines . lines
- where covInt s = read s :: Integer
-       getNum x = (splitOn " " x)!!1
-       printLines x = (putStrLn . show . sum . map covInt . map getNum) x
+    where covInt s = read s :: Integer
+    getNum x = (splitOn " " x)!!1
+    printLines x = (putStrLn . show . sum . map covInt . map getNum) x
+-}
+main = readFile "p2p.txt" >>= putStrLn . show . getSum . lines
+    where covInt s = read s :: Integer
+          getNum s = (splitOn " " s)!!1
+          getSum rows = (sum . map covInt . map getNum) rows
     
     
