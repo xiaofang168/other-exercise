189b84dbca6e3f9f73c7d50583f6f05141f1d9a9
fangjie <fangjie@etouch.cn>
2020-08-28 16:50:02 +0800

    not println

--- src/main/clojure/hello_test.clj
+++ src/main/clojure/hello_test.clj
@@ -1,7 +1,12 @@
 (println (+ 1 2))
 (println "Hello world.")
 
-(defn hello [name] 
-   (println "Hello," name))
+(defn hello [name]
+      (println "Hello," name))
 
 (hello "susan")
+
+(defn foo [a b c]
+      (* a b c))
+
+(foo 1 2 3)
