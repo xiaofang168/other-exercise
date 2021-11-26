78215b9b42ffdbe795b162672add7c646bf99d68
FangJie <jiefffang@didiglobal.com>
2021-11-26 11:08:43 +0800

    增量读取文件

---
+++ src/main/java/com/fangj/exercise/io/ReadFileSkipLine.java
@@ -0,0 +1,23 @@
+package com.fangj.exercise.io;
+
+import com.jramoyo.io.IndexedFileReader;
+
+import java.io.File;
+import java.util.SortedMap;
+
+/**
+ * @author fangjie
+ * @date Created in 7:28 下午 2021/11/25.
+ */
+public class ReadFileSkipLine {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File(ReadFileSkipLine.class.getResource("gc_read.log").getPath());
+        IndexedFileReader indexedFileReader = new IndexedFileReader(file);
+        SortedMap<Integer, String> integerStringSortedMap = indexedFileReader.readLines(16, 17);
+        integerStringSortedMap.forEach(
+                (k, v) -> System.out.println("key:value = " + k + ":" + v)
+        );
+    }
+
+}
