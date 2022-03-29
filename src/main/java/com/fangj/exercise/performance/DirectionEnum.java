e4345c4ffb6cf1984eec36d9c7d42632a48d204c
FangJie <jiefffang@didiglobal.com>
2022-03-29 19:10:19 +0800

    添加Java枚举EnumSet.allOf用法

---
+++ src/main/java/com/fangj/exercise/performance/DirectionEnum.java
@@ -0,0 +1,16 @@
+package com.fangj.exercise.performance;
+
+/**
+ * @author jefffang
+ * @date Created in 4:32 下午 2021/12/2.
+ */
+public enum DirectionEnum {
+    NORTH,
+    NORTHEAST,
+    EAST,
+    SOUTHEAST,
+    SOUTH,
+    SOUTHWEST,
+    WEST,
+    NORTHWEST
+}
