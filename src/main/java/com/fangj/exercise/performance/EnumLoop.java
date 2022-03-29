afbeb3fa73c9e3c1fe608cd9a804d42aafc91895
FangJie <jiefffang@didiglobal.com>
2022-03-29 19:10:19 +0800

    添加Java枚举EnumSet.allOf用法

---
+++ src/main/java/com/fangj/exercise/performance/EnumLoop.java
@@ -0,0 +1,21 @@
+package com.fangj.exercise.performance;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+/**
+ * @author fangjie
+ * @date Created in 4:32 下午 2021/12/2.
+ */
+public class EnumLoop {
+    public static void main(String[] args) {
+        DirectionEnum[] values = DirectionEnum.values();
+        DirectionEnum[] values2 = DirectionEnum.values();
+        DirectionEnum[] values3 = DirectionEnum.values();
+        System.out.println(values2==values3);
+        Set<DirectionEnum> directions = EnumSet.allOf(DirectionEnum.class);
+        for(DirectionEnum direction : directions) {
+            //System.out.println(direction);
+        }
+    }
+}
