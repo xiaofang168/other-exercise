14dd5cd837c4fdeaa820599c62bfcce18eec5233
FangJie <jiefffang@didiglobal.com>
2022-04-04 21:03:07 +0800

    增加了文件操作函数

--- src/main/clojure/file_io.clj
+++ src/main/clojure/file_io.clj
@@ -8,3 +8,27 @@
 (readFile "/Users/fangjie/github/other-exercise/src/main/clojure/macro_test.clj")
 
 (writeFile "/Users/fangjie/Downloads/aa.txt")
+
+;; 管道过程中一个函数的话括号可以省略
+(->> "/Users//gc_logs"
+     clojure.java.io/file
+     file-seq
+     (mapv str))
+
+;; doseq是副作用函数，总是返回nil
+(doseq [n (->> "/Users//gc_logs"
+               (clojure.java.io/file)
+               (file-seq)
+               (mapv str))]
+              (println n))
+
+;; Use the Path utilities to do glob filtering.
+(let [grammar-matcher (.getPathMatcher
+                        (java.nio.file.FileSystems/getDefault)
+                        "glob:*.{zip}")]
+     (->> "/Users//dev-tools"
+          clojure.java.io/file
+          file-seq
+          (filter #(.isFile %))
+          (filter #(.matches grammar-matcher (.getFileName (.toPath %))))
+          (mapv #(.getAbsolutePath %))))
