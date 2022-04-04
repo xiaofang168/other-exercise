0bec9992e6eca61afe906ae888af7267a4e6062d
FangJie <jiefffang@didiglobal.com>
2022-04-04 11:31:36 +0800

    增加了宏的使用

--- src/main/clojure/macro_test.clj
+++ src/main/clojure/macro_test.clj
@@ -2,7 +2,7 @@
 ; 宏编译器从字面上传递Clojure代码，对参数进行替换，不求值，替换发生在编译期，宏在运行时不存在
 
 (defmacro assert-equals [actual expected]
-			(= expected actual))
+          (= expected actual))
 
 ; true
 (println (assert-equals 2 2))
@@ -23,7 +23,7 @@
 (println (assert-equals (inc 5) 6))
 
 ; 输出宏替换，即要执行的程序代码
-; 在调试宏时， macroexpand和macroexpand-1选择的武器
+; 在调试宏时， macroexpand和macroexpand-1选择的武器，优先选择macroexpand-1，因为系统自带一些宏(defn)不需要了解
 (println (macroexpand '(assert-equals (inc 5) 6)))
 (println (macroexpand-1 '(assert-equals (inc 5) 6)))
 
@@ -32,22 +32,46 @@
 
 ; 以下两个函数是等效的
 (defmacro assert-equals [actual expected]
-    (list 'when-not (list '= actual expected)
-        (list 'throw
-            (list 'AssertionError.
-                (list 'str "Expected " expected " but was " actual)))))
+          (list 'when-not (list '= actual expected)
+                (list 'throw
+                      (list 'AssertionError.
+                            (list 'str "Expected " expected " but was " actual)))))
 
 ; (when-not (= (inc 5) 6) (throw (AssertionError. (str Expected  6  but was  (inc 5)))))
 (println (macroexpand-1 '(assert-equals (inc 5) 6)))
 
 ; `完成所有引用，~取消部分引用
+; syntax quoting会让代码更加简洁
+; syntax quoting会返回fully qualified symbol，所以使用syntax quoting能够让我们避免命名冲突
 (defmacro assert-equals [actual expected]
-    `(when-not (= ~actual ~expected)
-        (throw
-            (AssertionError.
-                (str "Expected " ~expected " but was " ~actual)))))
+          `(when-not (= ~actual ~expected)
+                     (throw
+                       (AssertionError.
+                         (str "Expected " ~expected " but was " ~actual)))))
 
 ; 用macroexpand-1来查看宏在编译过程中如何扩展
 ; macroexpand也可以工作，但是由于when-not也是一个宏（！），它将被递归扩展，从而使输出混乱
 (assert-equals 5 5)
 (println (macroexpand-1 '(assert-equals (inc 5) 7)))
+
+(defmacro my-plus
+          "Another plus for a + b"
+          [args]
+          (list (second args) (first args) (last args)))
+
+(macroexpand-1 '(my-plus (1 + 1)))
+(my-plus (1 + 1))
+
+; 'Simple Quoting和`Syntax Quoting的区别
+(defmacro my-print
+          [expression]
+          (list 'let ['result expression]
+                (list 'println 'result)
+                'result))
+
+; #不能去掉gensym宏内部使用的符号
+(defmacro my-print2
+          [expression]
+          `(let [result# ~expression]
+                (println result#)
+                result#))
