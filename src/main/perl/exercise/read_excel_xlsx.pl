a2cf06b919378f4630ad8cd8e342239d718287e3
FangJie <jiefffang@didiglobal.com>
2022-04-09 12:34:29 +0800

    增加了一些语法使用说明

--- src/main/perl/exercise/read_excel_xlsx.pl
+++ src/main/perl/exercise/read_excel_xlsx.pl
@@ -5,7 +5,26 @@ use Spreadsheet::ParseExcel;
 use Spreadsheet::XLSX;
 
 my $excel = Spreadsheet::XLSX->new('test.xlsx');
+=pod
+对@array的解引用：
+$$reference[element];
+$reference->[element];
+@$reference; #to access the whole array
+@{reference}
 
+对%hash的解引用：
+$$reference{'key'};
+$reference->{'key'};
+%$reference; #to access the whole hash
+=cut
+
+# 遍历数组中的hash引用(引用本身是标量)
+# 对象只是一个引用（也就是一个scalar变量），它恰好知道自己属于哪个类。要告诉一个引用它所指向的内容属于哪个类
+# scalar变量能包含：
+# undef（对应Python中的None、PHP中的null）
+# 数值（Perl不区分整形和浮点类型）
+# 字符串
+# 其他变量的引用
 foreach my $sheet (@{$excel->{Worksheet}}) {
 
     printf("Sheet: %s\n", $sheet->{Name});
