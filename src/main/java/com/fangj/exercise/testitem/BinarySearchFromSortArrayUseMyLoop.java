e661e16811d283e4cf980f775c7c49320dadcd2a
FangJie <jiefffang@didiglobal.com>
2022-04-20 13:21:20 +0800

    二分查找其他写法,左右移动位置

---
+++ src/main/java/com/fangj/exercise/testitem/BinarySearchFromSortArrayUseMyLoop.java
@@ -0,0 +1,67 @@
+package com.fangj.exercise.testitem;
+
+/**
+ * @author fangjie
+ * @date Created in 下午9:29 2022/4/19.
+ */
+public class BinarySearchFromSortArrayUseMyLoop {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 20, 25, 27, 30, 31, 32, 35, 57, 63, 81, 84, 101, 102, 108, 200, 202, 301};
+        for (int i = 0; i < array.length; i++) {
+            int indexNum = search(array, array[i]);
+            System.out.println("循环查找" + array[i] + "索引结果>>>" + indexNum);
+        }
+        System.out.println("====end====");
+    }
+
+    public static int search(int[] array, int dest) {
+        if (array == null) {
+            return -1;
+        }
+        int cursorIndex = array.length / 2;
+        int middleValue;
+        // 右侧区间遍历
+        if (dest > array[cursorIndex]) {
+            int c = 0;
+            while (true) {
+                middleValue = array[cursorIndex];
+                // 右区间指针
+                if (dest > middleValue) {
+                    // 往右走的距离
+                    c = (array.length - cursorIndex) / 2;
+                    cursorIndex = cursorIndex + c;
+                } else if (dest < middleValue) {
+                    // 往左回修正
+                    c = c / 2 == 0 ? 1 : c / 2;
+                    cursorIndex = cursorIndex - c;
+                }
+                if (dest == array[cursorIndex]) {
+                    return cursorIndex;
+                }
+            }
+        } else if (dest < array[cursorIndex]) {
+            int c = 0;
+            // 左侧区间遍历
+            while (true) {
+                middleValue = array[cursorIndex];
+                // 左区间指针
+                if (dest < middleValue) {
+                    // 往左走的距离
+                    c = cursorIndex / 2;
+                    cursorIndex = cursorIndex / 2;
+                } else if (dest > middleValue) {
+                    // 往右走修正
+                    c = c / 2 == 0 ? 1 : c / 2;
+                    cursorIndex = cursorIndex + c;
+                }
+                if (dest == array[cursorIndex]) {
+                    return cursorIndex;
+                }
+            }
+        }
+        return cursorIndex;
+    }
+
+}
