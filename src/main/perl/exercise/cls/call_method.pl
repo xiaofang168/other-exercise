7d643539b48e77ec08fec48693ce4a5fac4ea7ce
FangJie <jiefffang@didiglobal.com>
2022-04-26 22:14:35 +0800

    面向对象类方法调用

---
+++ src/main/perl/exercise/cls/call_method.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl 
+use warnings;
+use strict;
+# 引入类文件，采用相对路径
+use lib ".";
+
+use Person;
+my $object = new Person();
+
+$object->foo(POST => '10.161.179.23:8083/data/commit', 'Content-Type' => 'application/json','dddd');
+print "\n\n";
+
+# 和上面的写法一样
+$object->foo('POST','10.161.179.23:8083/data/commit', 'Content-Type','application/json','dddd');
