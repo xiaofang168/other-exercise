9352b3885ce1b78eb44b3a2287c845d7d03abf9f
FangJie <jiefffang@didiglobal.com>
2022-05-13 12:48:28 +0800

    简单的一些语法测试

---
+++ src/main/python/code_test.py
@@ -0,0 +1,14 @@
+# coding=UTF-8
+# 字符编码设置
+
+# 字符串格式化 %-formatting(最早 Python 的格式化字串)
+# %-formatting、str-format（Python 2.6+）、f-string （Python 3.6+）
+str = r"%s-%s.jpg" % (2,4)
+print(str)
+
+temp = "I am {first_name} {middle_name}. {last_name}".format(
+     first_name='li',
+     middle_name='shi',
+     last_name='min'
+)
+print(temp)
