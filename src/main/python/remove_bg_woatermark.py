7572be997be162784cb13404c028be8a3f9a204f
FangJie <jiefffang@didiglobal.com>
2022-05-17 11:29:08 +0800

    去除图片背景水印

---
+++ src/main/python/remove_bg_woatermark.py
@@ -0,0 +1,18 @@
+# coding=UTF-8
+import cv2
+import numpy as np
+import os
+
+dir = os.getcwd()
+print(dir)
+   
+
+img = cv2.imread("add_mark/WX20220302-202232@2x.jpg")
+
+alpha = 2.0
+beta = -160
+
+new = alpha * img + beta
+new = np.clip(new, 0, 255).astype(np.uint8)
+
+cv2.imwrite("cleaned.png", new)
