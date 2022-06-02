22bbccc862abcb509cd75f67eec9b86665426135
FangJie <jiefffang@didiglobal.com>
2022-06-02 20:06:50 +0800

    增加http的使用,去掉换行符

--- src/main/python/file/read_file_split.py
+++ src/main/python/file/read_file_split.py
@@ -3,7 +3,7 @@ import re
 with open('dk.txt', encoding="utf-8") as f:
 	for line in f:
 		# 正则表达式空格分隔
-		data = re.split('\\s+', line, 2)
+		data = re.split('\\s+', line.rstrip(), 2)
 
 		# f格式化字符串
 		print(f"{data[1]}, {data[2]}")
