7b0d3ab4fded65f664e0e2a62c7f7eb5135868b2
FangJie <jiefffang@didiglobal.com>
2022-06-10 22:06:58 +0800

    使用百度飞桨OCR识别图片文字

---
+++ src/main/python/ai/ocr_with_easyocr.py
@@ -0,0 +1,12 @@
+# coding=UTF-8
+# 导入easyocr
+import easyocr
+# 创建reader对象
+reader = easyocr.Reader(['ch_sim','en']) 
+# 读取图像
+result = reader.readtext('/Users/didi/Downloads/费曼技巧.jpeg')
+print(result)
+# 结果不如百度飞桨paddleocr
+for i in result:
+	word = i[1]
+	print(word)
